6ca6913ae97febf39cf5d011641ce6558cdae306
Travis CI User <travis@example.org>
2018-08-13 18:59:34 +0000

    Update docs after building Travis build 582 of pcdshub/pcdsdevices
    The docs were built from the branch 'master' against the commit
    fba1e6dbe2c528f652c074c1a6329e4e0c95efc4.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pcdshub/pcdsdevices/jobs/415588299.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.0+159.g954025f',
+    VERSION: '0.8.0+175.gfba1e6d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
